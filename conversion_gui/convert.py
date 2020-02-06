9710828d60377dfa712054dcfc25c8b32bb731c5
Bryan Reese <bryan.mreese@gmail.com>
2020-02-06 14:03:54 -0500

    Did a quick GUI and converion to learn ttk

---
+++ conversion_gui/convert.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+from tkinter import *
+from tkinter import ttk
+
+def convert(*args):
+    try:
+        num = float(entered.get())
+        meters.set(num / 3.2808)
+        feet.set(num * 3.2808)
+        print("Meters to Feet:", meters.get(), "--- Feet to Meters:", feet.get())
+    except ValueError:
+        print("Did you enter a number? Value entered:", entered.get())
+        pass
+    finally:
+        entry.focus()
+
+# Create window and inner frame
+window = Tk()
+window.title("Feet to meters, and meters to feet...")
+mainframe = ttk.Frame(window, padding="10 10 24 24")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+# Variables
+entered = StringVar()
+feet = StringVar()
+meters = StringVar()
+
+# Entry
+entry = ttk.Entry(mainframe, width=10, textvariable=entered)
+entry.grid(column=2, row=1)
+ttk.Button(mainframe, text="Convert", command=convert).grid(column=3, row=1)
+ttk.Label(mainframe, text="Value:").grid(column=1, row=1)
+
+#Display
+ttk.Label(mainframe, text="Meters to Feet: ").grid(column=1, row=2)
+ttk.Label(mainframe, textvariable=feet).grid(column=2, row=2)
+ttk.Label(mainframe, text="Feet to Meters: ").grid(column=1, row=3)
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=3)
+
+# Focus input and accept return, then make the GUI
+entry.focus()
+window.bind('<Return>', convert)
+window.mainloop()
