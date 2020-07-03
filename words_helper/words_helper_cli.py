8402e095f7bf0aa7ebbc5189692dbcf32861f9fb
Bryan Reese <bryan.mreese@gmail.com>
2020-07-03 18:32:46 -0400

    Made Windows work

--- words_helper/words_helper_cli.py
+++ words_helper/words_helper_cli.py
@@ -2,13 +2,22 @@
 from collections import Counter
 from pathlib import Path
 import sys
+from os.path import expanduser
 
 if len(sys.argv) > 1:
     letters = sys.argv[1]
 else:
     letters = input("Input your letters and we will make words:\n").lower()
 
-dictionary = Path('/usr/share/dict/words').read_text().lower().splitlines()
+try:
+    dictionary = Path(expanduser('~')+'/words.txt').read_text().lower().splitlines()
+except:
+    try:
+        dictionary = Path('/usr/share/dict/words').read_text().lower().splitlines()
+    except:
+        print('Dictionary not found at /usr/share/dict/words')
+        print('Dictionary not found at '+expanuser('~')+'/words.txt')
+        sys.exit()
 
 def anagramSet(dictionary, letters):
     anagrams = set()
