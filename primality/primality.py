454656ac5952fc95646e2a64e4e395ce510e26e6
Bryan Reese <bryan.mreese@gmail.com>
2020-07-30 17:54:17 -0400

    Added better handling for big numbers

--- primality/primality.py
+++ primality/primality.py
@@ -4,14 +4,23 @@ import sys
 def findPrime(number):
     print('Finding if', number, 'is prime.')
 
-    sieve = sieveOfEratosthenes(number)
+    if number < 10000000:
+        sieve = sieveOfEratosthenes(number)
 
-    for x in sieve:
-        if (number % x == 0):
-            print(number, 'is divisible by', x)
-            sys.exit()
+        for x in sieve:
+            if (number % x == 0):
+                print(number, 'is divisible by', x)
+                sys.exit()
 
-    print('Could not find a divisor,', number, 'must be prime.')
+        print('Could not find a divisor,', number, 'must be prime.')
+    else:
+        shortPrimes = set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
+        for n in shortPrimes:
+            if (number % n == 0):
+                print(number, 'is divisible by', n)
+                sys.exit()
+        print('Number could be prime,', number, 'is too large to sieve')
+        print('Checked if divisible by:', shortPrimes)
 
 def sieveOfEratosthenes(number):
     prime = set(range(2, number))
